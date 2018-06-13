2c6d081e8a7f424c1d076f9d7482c6e2734c93ab
Jacob Bair <jacob.bair@ivanti.com>
2018-06-13 08:30:04 -0600

    fixed protocol

--- cordova/server/main.js
+++ cordova/server/main.js
@@ -8,7 +8,7 @@ function createWindow() {
     win = new BrowserWindow({ width: 800, height: 600 });
     win.loadURL(url.format({
         pathname: path.join(__dirname, '../www/index.html'),
-        protocol: 'file',
+        protocol: 'file:',
         slashes: true
     }));
 
